6acb339cafbcfd164c596657bf6706067457d4a4
Cecilia Voloski <ceciliavoloski@gmail.com>
2022-01-18 00:51:53 -0300

    tela de carregamento e spinner

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CoursesService } from '../services/courses.service';
 import { CourseInterface } from '../model/courseInterface';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -10,14 +10,14 @@ import { CourseInterface } from '../model/courseInterface';
 })
 export class CoursesComponent implements OnInit {
   displayedColumns = ['name', 'category'];
-  coursesDB: Observable<CourseInterface[]>; // é aquele "dataSource" do HTML
+  courses$: Observable<CourseInterface[]>; // é aquele "dataSource" do HTML
 
   //coursesService: CoursesService; Só consido declarar essa variavel no constructor, por causa do Injectable no courses.services
 
   constructor(private coursesService: CoursesService) {
     // this.coursesDB = [];  é o courses acima, precisa inicializar ele dentro do constructor
     //this.coursesServices = new CoursesServices();
-    this.coursesDB = this.coursesService.myList();
+    this.courses$ = this.coursesService.myList();
   }
 
   ngOnInit() {}
